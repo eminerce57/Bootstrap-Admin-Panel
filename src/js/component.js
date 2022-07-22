3c082c534202e0ca37ac193faaedf8f172aee2cf
eminerce57 <83703865+eminerce57@users.noreply.github.com>
2022-07-22 19:23:22 +0300

    update new js layer added request.js

---
+++ src/js/component.js
@@ -0,0 +1,14 @@
+// <li class="nav-item">
+// <a class="nav-link  active" href="../pages/dashboard.html">
+//   <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+    
+//   </div>
+//   <span class="nav-link-text ms-1">Dashboard</span>
+// </a>
+// </li>
+
+
+function sidebar(){
+
+
+}
