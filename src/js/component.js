894efe06750ad2c147e0e28aee9ac7568bd5123f
Emre Çalışkan <yusufemreclskn@gmail.com>
2022-07-23 06:05:15 +0300

    Added New Icons

--- src/js/component.js
+++ src/js/component.js
@@ -8,7 +8,7 @@ function sidebar(){
          <li class="nav-item">
          <a class="nav-link ${checkActive(sidebar.href)} " href="${sidebar.href}">
          <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
-         ${sidebar.icon}
+          <i class="${sidebar.icon}"></i>
         </div>
         <span class="nav-link-text ms-1">${sidebar.title}</span>
             </a>
