ca2bff0b00e14fe54f762943371370f6240532dd
eminerce57 <83703865+eminerce57@users.noreply.github.com>
2022-08-08 20:34:04 +0300

    update new function

--- app/js/index.js
+++ app/js/index.js
@@ -1,4 +1,3 @@
+const { log } = console; // console.log yazmaya gerek yok artık kullanım şekli log("lorem")
 
-
-
-const request = new Request
+const request = new Request // request promise i tanımlıyoruz
