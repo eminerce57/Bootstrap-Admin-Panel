99f4ded171b2d3533269698880529abfb2064610
eminerce57 <83703865+eminerce57@users.noreply.github.com>
2022-08-08 16:30:31 +0300

    update new alert funtion in table page

--- app/js/table.js
+++ app/js/table.js
@@ -13,8 +13,8 @@ table.innerHTML +=`
 <td>${user.status}</td>
 <td>${user.salary}</td>
 <td>
-<button onclick="dangerAlert()" class="btn btn-danger t-button"><i class="fa-solid fa-trash-can"></i></button>
-<button onclick="updateModal()" class="btn btn-info t-button"><i class="fa-solid fa-pen"></i></button>
+<button onclick="dangerAlert('${user.name} ${user.surname} ')" class="btn btn-danger t-button"><i class="fa-solid fa-trash-can"></i></button>
+<button onclick="updateModal()" class="btn btn-info t-button "><i class="fa-solid fa-pen"></i></button>
 </td>
 
 </tr>
@@ -33,4 +33,30 @@ setTimeout(function(){
 
     $('#table_id').DataTable();
 },100)
+}
+
+
+function dangerAlert(name){
+
+    Swal.fire({
+        title: `Do you want to delete ${name}`,
+        showDenyButton: true,
+        confirmButtonText: 'Yes',
+        denyButtonText: 'No',
+        customClass: {
+          actions: 'my-actions',
+          denyButton: 'order-3',
+          confirmButton: 'order-2',
+        
+        }
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire('Saved!', '', 'success')
+        } else if (result.isDenied) {
+          Swal.fire('not saved', '', 'info')
+        }
+      })
+
+
+
 }
