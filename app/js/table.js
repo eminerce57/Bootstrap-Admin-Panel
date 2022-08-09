194d2504d94d8df62dc553fa4d36cb14778101b5
eminerce57 <83703865+eminerce57@users.noreply.github.com>
2022-08-09 19:34:00 +0300

    I upated fix modal

--- app/js/table.js
+++ app/js/table.js
@@ -1,6 +1,10 @@
 
+
+
 getUserData()
 function getUserData(){
+
+
 let table = document.getElementById("table-body")
 request.get("js/json/example-user-data.json").then((response)=>{
 
@@ -14,7 +18,7 @@ table.innerHTML +=`
 <td>${user.salary}</td>
 <td>
 <button onclick="dangerAlert('${user.name} ${user.surname} ')" class="btn btn-danger t-button"><i class="fa-solid fa-trash-can"></i></button>
-<button onclick="updateModal()" class="btn btn-info t-button "><i class="fa-solid fa-pen"></i></button>
+<button onclick="updateModal('${user.name}','${user.surname}','${user.salary}')" class="btn btn-info t-button "><i class="fa-solid fa-pen"></i></button>
 </td>
 
 </tr>
@@ -30,34 +34,46 @@ console.log(error)
 })
 
 setTimeout(function(){
-
     $('#table_id').DataTable();
 },100)
 }
 
 
 function dangerAlert(name){
+  if(name == undefined){
+    
+    name="?"
 
-    Swal.fire({
-        title: `Do you want to delete ${name}`,
+  }
  
-        showDenyButton: true,
-        confirmButtonText: 'Yes',
-        denyButtonText: 'No',
-        customClass: {
-          actions: 'my-actions',
-          denyButton: 'order-3',
-          confirmButton: 'order-2',
-        
-        }
-      }).then((result) => {
-        if (result.isConfirmed) {
-          Swal.fire('Saved!', '', 'success')
-        } else if (result.isDenied) {
-          Swal.fire('not saved', '', 'info')
-        }
-      })
+
+  Swal.fire({
+    title: `Do you want to delete ${name}`,
+
+    showDenyButton: true,
+    confirmButtonText: 'Yes',
+    denyButtonText: 'No',
+    customClass: {
+      actions: 'my-actions',
+      denyButton: 'order-3',
+      confirmButton: 'order-2',
+    
+    }
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire('Saved!', '', 'success')
+    } else if (result.isDenied) {
+      Swal.fire('not saved', '', 'info')
+    }
+  })
 
 
+}
 
+
+function updateModal(name,surname,salary){
+  $("#Modal").modal();
+  document.getElementById("name").value =name
+  document.getElementById("surname").value =surname
+  document.getElementById("salary").value =salary
 }
