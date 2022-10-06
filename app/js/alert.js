9ee3c077cb114e48104cc6417f11f94136ccf2e3
eminerce57 <83703865+eminerce57@users.noreply.github.com>
2022-10-06 09:51:49 +0300

    update

--- app/js/alert.js
+++ app/js/alert.js
@@ -15,14 +15,65 @@ console.log(e.target.id)
 if(e.target.id == "default"){
 alert("alert")
 }
+
 else if(e.target.id == "custom"){
 
     Swal.fire({
-        title: `Custom alert`,
+        icon: 'success',
+        title: 'Your work has been saved',
+        showConfirmButton: false,
+        timer: 1500,
         background: '#19191A',
-    
-    
+
     })
+    
+
+}
+else if(e.target.id == "ask"){
+
+    Swal.fire({
+        title: `ask`,
+        background: '#19191A',
+        showDenyButton: true,
+        confirmButtonText: 'Yes',
+        denyButtonText: 'No',
+        customClass: {
+        actions: 'my-actions',
+        denyButton: 'order-3',
+        confirmButton: 'order-2',
+        
+        }
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire('Saved!', '', 'success')
+        } else if (result.isDenied) {
+          Swal.fire('not saved', '', 'info')
+        }
+      })
+    
+
+}
+
+else if(e.target.id == "or"){
+    Swal.fire({
+        title: `Or`,
+        background: '#19191A',
+        showDenyButton: true,
+        confirmButtonText: 'Yes',
+        denyButtonText: 'No',
+        customClass: {
+        actions: 'my-actions',
+        denyButton: 'order-3',
+        confirmButton: 'order-2',
+        
+        }
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire('Saved!', '', 'success')
+        } else if (result.isDenied) {
+          Swal.fire('not saved', '', 'info')
+        }
+      })
     
 
 }
